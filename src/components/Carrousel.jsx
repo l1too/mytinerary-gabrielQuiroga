a2354323d53195fa16056d27e28077406429defa
Gabriel Quiroga <gabrielnahuel.quiroga@gmail.com>
2023-08-08 12:11:12 -0300

    actualizacion de carrusel 2

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
 
 const Carrousel = () => {
+
+    const cities = [
+        {name: 'Rome',
+        img: '/rome.jpg'},
+        {name: 'Moscow',
+        img: '/moscow.jpg'},
+        {name: 'Venice',
+        img: '/venice.jpg'},
+        {name: 'London',
+        img: '/london.jpg'},
+        {name: 'Liguria',
+        img: '/liguria.jpg'},
+        {name: 'Copenhagen',
+        img: '/copenhagen.jpg'},
+        {name: 'Tokyo',
+        img: '/tokyo.jpg'},
+        {name: 'Alexandria',
+        img: '/alexandria.jpg'},
+        {name: 'Madrid',
+        img: '/madrid.jpg'},
+        {name: 'Paris',
+        img: '/paris.jpg'},
+        {name: 'Santa Cruz',
+        img: '/santa-cruz.jpg'},
+        {name: 'Colmar',
+        img: '/colmar.jpg'}
+    ]
+
   return (
 
     <div className='flex justify-center h-100 '>
@@ -12,20 +40,20 @@ const Carrousel = () => {
                     <a href="#slide3" className="btn btn-circle my-auto">❮</a> 
                         <div className='columns-2 my-auto '>
                             <div className='w-32 h-52 mb-5 relative'>
-                                <img src="/moscu.jpg" className=" object-cover h-full w-full " />
-                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>Visit Moscu</h3>
+                                <img src={cities[0].img} className=" object-cover h-full w-full " />
+                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>{`Visit ${cities[0].name}`}</h3>
                             </div>
                             <div className='w-32 h-52 mb-5 relative'>
-                                <img src="/venecia.jpg" className="object-cover h-full w-full " />
-                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>Visit Moscu</h3>
+                            <img src={cities[1].img} className=" object-cover h-full w-full " />
+                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>{`Visit ${cities[1].name}`}</h3>
                             </div>
                             <div className='w-32 h-52 mb-5 relative'>
-                                <img src="/paris.jpg" className="object-cover h-full w-full " />
-                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>Visit Moscu</h3>
+                            <img src={cities[2].img} className=" object-cover h-full w-full " />
+                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>{`Visit ${cities[2].name}`}</h3>
                             </div>
                             <div className='w-32 h-52 mb-5 relative'>
-                                <img src="/madrid.jpg" className="object-cover h-full w-full " />
-                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>Visit Moscu</h3>
+                            <img src={cities[3].img} className=" object-cover h-full w-full " />
+                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>{`Visit ${cities[3].name}`}</h3>
                             </div>
                         </div>
                     <a href="#slide2" className="btn btn-circle my-auto">❯</a>
@@ -39,20 +67,20 @@ const Carrousel = () => {
                     <a href="#slide1" className="btn btn-circle my-auto">❮</a> 
                         <div className='columns-2 my-auto '>
                             <div className='w-32 h-52 mb-5 relative'>
-                                <img src="/london.jpg" className=" object-cover h-full w-full " />
-                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>Visit Moscu</h3>
+                            <img src={cities[4].img} className=" object-cover h-full w-full " />
+                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>{`Visit ${cities[4].name}`}</h3>
                             </div>
                             <div className='w-32 h-52 mb-5 relative'>
-                                <img src="/santa-cruz.jpg" className="object-cover h-full w-full " />
-                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>Visit Moscu</h3>
+                            <img src={cities[5].img} className=" object-cover h-full w-full " />
+                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>{`Visit ${cities[5].name}`}</h3>
                             </div>
                             <div className='w-32 h-52 mb-5 relative'>
-                                <img src="/roma.jpg" className="object-cover h-full w-full " />
-                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>Visit Moscu</h3>
+                            <img src={cities[6].img} className=" object-cover h-full w-full " />
+                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>{`Visit ${cities[6].name}`}</h3>
                             </div>
                             <div className='w-32 h-52 mb-5 relative'>
-                                <img src="/copenhagen.jpg" className="object-cover h-full w-full " />
-                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>Visit Moscu</h3>
+                            <img src={cities[7].img} className=" object-cover h-full w-full " />
+                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>{`Visit ${cities[7].name}`}</h3>
                             </div>
                         </div>
                     <a href="#slide3" className="btn btn-circle my-auto">❯</a>
@@ -65,20 +93,20 @@ const Carrousel = () => {
                     <a href="#slide2" className="btn btn-circle my-auto">❮</a> 
                         <div className='columns-2 my-auto '>
                             <div className='w-32 h-52 mb-5 relative'>
-                                <img src="/colmar.jpg" className=" object-cover h-full w-full " />
-                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>Visit Moscu</h3>
+                            <img src={cities[8].img} className=" object-cover h-full w-full " />
+                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>{`Visit ${cities[8].name}`}</h3>
                             </div>
                             <div className='w-32 h-52 mb-5 relative'>
-                                <img src="/tokyo.jpg" className="object-cover h-full w-full " />
-                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>Visit Moscu</h3>
+                            <img src={cities[9].img} className=" object-cover h-full w-full " />
+                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>{`Visit ${cities[9].name}`}</h3>
                             </div>
                             <div className='w-32 h-52 mb-5 relative'>
-                                <img src="/liguria.jpg" className="object-cover h-full w-full " />
-                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>Visit Moscu</h3>
+                            <img src={cities[10].img} className=" object-cover h-full w-full " />
+                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>{`Visit ${cities[10].name}`}</h3>
                             </div>
                             <div className='w-32 h-52 mb-5 relative'>
-                                <img src="/alejandria.jpg" className="object-cover h-full w-full " />
-                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>Visit Moscu</h3>
+                            <img src={cities[11].img} className=" object-cover h-full w-full " />
+                                <h3 className='text-center absolute bottom-1 bg-slate-600 text-white w-full'>{`Visit ${cities[11].name}`}</h3>
                             </div>
                         </div>
                     <a href="#slide1" className="btn btn-circle my-auto">❯</a>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
       </div>
     </div> 
     <div>
-      <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+      <p>Copyright © 2023 - All right reserved by Quiroga Gabriel</p>
     </div>
   </footer>
   )
